2fd526b776df5dfe58c468c9fb72d730fffc575b
YvesLoic <yvestchoumteu55@gmail.com>
2022-02-18 15:28:26 +0100

    Protection des routes API

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'userIs' => \App\Http\Middleware\UserIs::class,
+        'jwt' => \App\Http\Middleware\JwtMiddleware::class,
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,4 +20,12 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource("user", UserController::class);
+Route::prefix('auth')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('jwt');
+    Route::post('refresh', [AuthController::class, 'refresh'])->middleware('jwt');
+});
+
+Route::group(['middleware' => 'jwt'], function () {
+    Route::apiResource("user", UserController::class);
+});
